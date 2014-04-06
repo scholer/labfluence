2337bd0a7f1d6bb96e035f97281cd960609c789e
Rasmus Scholer (TimelineX) <rasmusscholer@gmail.com>
2014-04-06 23:07:24 +0200

    (Should have been included in last commit) Change to cache_decorator: implemented set and delete descriptor protocol. Setting the a cached property will set the cache value, while deleting it will delete the cache entry (it will NOT delete the property per se).

---
+++ model/decorators/cache_decorator_org.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+##    Copyright 2013 Rasmus Scholer Sorensen, rasmusscholer@gmail.com
+##
+##    This program is free software: you can redistribute it and/or modify
+##    it under the terms of the GNU General Public License as published by
+##    the Free Software Foundation, either version 3 of the License, or
+##    (at your option) any later version.
+##
+##    This program is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License for more details.
+##
+##    You should have received a copy of the GNU General Public License
+##
+
+"""
+
+A minor note about python decorators:
+They are defined as though they were classes.
+They implement e.g. __init__ , __call__
+(similar to what you could do in a normal object -- although you usually only override __init__)
+and __get__
+Example (PS: only works for new-style objects; uses mixin I think...)
+
+
+"""
+
+#
+# © 2011 Christopher Arndt, MIT License
+# From https://wiki.python.org/moin/PythonDecoratorLibrary#Cached_Properties
+
+import time
+
+class cached_property(object):
+    '''Decorator for read-only properties evaluated only once within TTL period.
+
+    It can be used to created a cached property like this::
+
+        import random
+
+        # the class containing the property must be a new-style class
+        class MyClass(object):
+            # create property whose value is cached for ten minutes
+            @cached_property(ttl=600)
+            def randint(self):
+                # will only be evaluated every 10 min. at maximum.
+                return random.randint(0, 100)
+
+    The value is cached  in the '_cache' attribute of the object instance that
+    has the property getter method wrapped by this decorator. The '_cache'
+    attribute value is a dictionary which has a key for every property of the
+    object which is wrapped by this decorator. Each entry in the cache is
+    created only when the property is accessed for the first time and is a
+    two-element tuple with the last computed property value and the last time
+    it was updated in seconds since the epoch.
+
+    The default time-to-live (TTL) is 300 seconds (5 minutes). Set the TTL to
+    zero for the cached value to never expire.
+
+    To expire a cached property value manually just do::
+
+        del instance._cache[<property name>]
+
+    '''
+    def __init__(self, ttl=300):
+        self.ttl = ttl
+
+    def __call__(self, fget, doc=None):
+        self.fget = fget
+        self.__doc__ = doc or fget.__doc__
+        self.__name__ = fget.__name__
+        self.__module__ = fget.__module__
+        return self
+
+    def __get__(self, inst, owner):
+        ## Note: putting now = time.time() at the top will make the cache behavior dependent on the calculation time.
+        ## You could argue the cache time-to-live should be calculated from _after_ the calculation
+        ## has finished, not from when it was started.
+        ## (This is only important if calculation time (self.fget) is comparable to self.ttl)
+        now = time.time()
+        try:
+            value, last_update = inst._cache[self.__name__]
+            if self.ttl > 0 and now - last_update > self.ttl:
+                raise AttributeError
+        except (KeyError, AttributeError):
+            value = self.fget(inst)
+            try:
+                cache = inst._cache
+            except AttributeError:
+                cache = inst._cache = {} # empty dict.
+            cache[self.__name__] = (value, now) # Consider calling for another time.time() ?
+        return value
